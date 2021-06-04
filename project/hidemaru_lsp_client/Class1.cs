3b7eb6802bc1c29679b16612e2ed8d86d8521382
ohtorii <ikeuchi.h9@gmail.com>
2021-06-04 15:31:45 +0900

    Add hidemaru-lsp-client project.

---
+++ project/hidemaru_lsp_client/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hidemaru_lsp_client
+{
+    public class Class1
+    {
+    }
+}
