d7006cf4b4fea35a2ff7688b02b66b1c1217530a
ohtorii <ikeuchi.h9@gmail.com>
2021-08-29 11:46:54 +0900

    Add Util.cs

---
+++ project/LanguageServerProtocol/Client/Util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSP.Implementation
+{
+    class Util
+    {
+        /// <summary>
+        /// URIを正規化する        
+        /// (Ex.)
+        /// Before:file:///c%3A/Users/foo/GitHub/hidemaru_lsp_client/project/TestData/lua/test1.lua
+        /// After :file:///c:/Users/foo/GitHub/hidemaru_lsp_client/project/Testdata/lua/test1.lua			
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        static public string NormalizeUri(string uri)
+        {
+            return Uri.UnescapeDataString(uri);
+        }
+    }
+}
