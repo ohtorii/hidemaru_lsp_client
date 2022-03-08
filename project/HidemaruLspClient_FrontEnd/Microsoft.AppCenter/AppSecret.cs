d3414bf0b73969ad00a08e12c7d9081d833bec0d
ohtorii <ikeuchi.h9@gmail.com>
2022-03-08 17:38:56 +0900

    refactoring

--- project/HidemaruLspClient_FrontEnd/Microsoft.AppCenter/AppSecret.cs
+++ project/HidemaruLspClient_FrontEnd/Microsoft.AppCenter/AppSecret.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HidemaruLspClient_FrontEnd
+﻿namespace HidemaruLspClient_FrontEnd
 {
     partial class MicrosoftAppCenter
     {
