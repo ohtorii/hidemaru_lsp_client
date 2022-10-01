44a1d7fdaa0b5ca3d36d698d03da90c17a41afc2
ohtorii <ikeuchi.h9@gmail.com>
2022-10-01 20:31:38 +0900

    Update Compiler.cs

--- project/HidemaruLspClient_FrontEnd/DynamicRun/Compiler.cs
+++ project/HidemaruLspClient_FrontEnd/DynamicRun/Compiler.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using HidemaruLspClient_BackEndContract;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
@@ -14,42 +15,34 @@ namespace HidemaruLspClient_FrontEnd.DynamicRun
 {
     internal class Compiler
     {
-        public byte[] CompillingString(string sourceCode)
+        public static byte[] CompillingString(string sourceCode, ILspClientLogger logger=null)
         {
             using (var peStream = new MemoryStream())
             {
-                var result = GenerateCode(sourceCode).Emit(peStream);
-
+                var result = GenerateCode(sourceCode,logger).Emit(peStream);
                 if (!result.Success)
                 {
-                    Console.WriteLine("Compilation done with error.");
-
-                    var failures = result.Diagnostics.Where(diagnostic => diagnostic.IsWarningAsError || diagnostic.Severity == DiagnosticSeverity.Error);
-
+                    logger?.Error("Compilation done with error.");
+                    var failures = result.Diagnostics.Where(diagnostic => diagnostic.IsWarningAsError || diagnostic.Severity == Microsoft.CodeAnalysis.DiagnosticSeverity.Error);
                     foreach (var diagnostic in failures)
                     {
-                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                        logger?.Error(String.Format("{0}: {1}", diagnostic.Id, diagnostic.GetMessage()));
                     }
-
                     return null;
                 }
-
-                Console.WriteLine("Compilation done without any error.");
-
+                //Console.WriteLine("Compilation done without any error.");
                 peStream.Seek(0, SeekOrigin.Begin);
-
                 return peStream.ToArray();
             }
         }
-        public byte[] CompilingFile(string filepath)
+        public static byte[] CompilingFile(string filepath, ILspClientLogger logger = null)
         {
-            Console.WriteLine($"Starting compilation of: '{filepath}'");
-
+            logger?.Info(String.Format($"Starting compilation of: '{filepath}'"));
             var sourceCode = File.ReadAllText(filepath);
-            return CompillingString(sourceCode);
+            return CompillingString(sourceCode,logger);
         }
 
-        private static CSharpCompilation GenerateCode(string sourceCode)
+        private static CSharpCompilation GenerateCode(string sourceCode, ILspClientLogger logger)
         {
             var codeString = SourceText.From(sourceCode);
             var options = CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp10);
@@ -62,9 +55,33 @@ namespace HidemaruLspClient_FrontEnd.DynamicRun
                 MetadataReference.CreateFromFile(typeof(Console).Assembly.Location)
             };
 
-            Assembly.GetEntryAssembly()?.GetReferencedAssemblies().ToList()
-                .ForEach(a => references.Add(MetadataReference.CreateFromFile(Assembly.Load(a).Location)));
+            /*
+            var items = Assembly.GetEntryAssembly()?.GetReferencedAssemblies().ToList();
+            if (items != null)
+            {
+                logger?.Debug(String.Format($"Assembly names. num={items.Count}"));
+                foreach (var item in items)
+                {
+                    logger?.Debug(item.FullName);
+                    references.Add(MetadataReference.CreateFromFile(Assembly.Load(item).Location));
+                }
+            }*/
 
+            /*
+            string[] ReferencedAssemblies = new[]
+            {
+                "mscorlib.dll",
+                "System.dll",
+                "System.Core.dll",
+                "System.Data.dll",
+                "System.Data.DataSetExtensions.dll",
+                "System.Xml.dll",
+                "System.Xml.Linq.dll",
+            };
+            foreach(var item in ReferencedAssemblies)
+            {
+                references.Add(MetadataReference.CreateFromFile(Assembly.Load(item).Location));
+            }*/
             return CSharpCompilation.Create("Hello.dll",
                 new[] { parsedSyntaxTree },
                 references: references,
