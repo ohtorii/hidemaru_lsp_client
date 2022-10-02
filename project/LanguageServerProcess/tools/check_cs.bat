f71afc5532da6ceb63b1f9f4931185523debd7b0
ohtorii <ikeuchi.h9@gmail.com>
2022-10-02 12:45:30 +0900

    Update check_cs.bat

--- project/LanguageServerProcess/tools/check_cs.bat
+++ project/LanguageServerProcess/tools/check_cs.bat
@@ -6,10 +6,13 @@ if "%1"=="" (
     exit /b 1
 )
 set INFILE=%1
-set CSC=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe 
 set OUT=%TEMP%\hidemaru_lsp_check_cs_TEMP.dll
 
-"%CSC%" /out:%OUT% /target:library /reference:%~dp0/../../../internal/bin/HidemaruLspClient_FrontEnd/LanguageServerProcess.dll "%INFILE%"
+REM "%CSC%"  /target:library /reference:%~dp0/../../../internal/bin/HidemaruLspClient_FrontEnd/LanguageServerProcess.dll "%INFILE%"
+
+
+dotnet "C:\Program Files\dotnet\sdk\5.0.214\Roslyn\bincore\csc.dll" -r:"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\5.0.17\System.Private.CoreLib.dll" -r:"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\5.0.17\System.IO.dll" -r:"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\5.0.17\System.Runtime.dll" -r:"%~dp0/../../../project\LanguageServerProcess\bin\x64\Release\net5.0-windows\LanguageServerProcess.dll" /out:"%OUT%" /target:library "%INFILE%"
+
 if "%ERRORLEVEL%"=="0" (
     echo =====================================
 	echo ï∂ñ@ÉGÉâÅ[ÇÕå©Ç¬Ç©ÇËÇ‹ÇπÇÒÇ≈ÇµÇΩ
